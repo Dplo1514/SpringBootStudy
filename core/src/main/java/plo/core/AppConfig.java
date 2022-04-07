5b9e6892dff9ea553715eef8400ba3fe7cf03ba6
Dplo1514 <dladlsgur3334@gmail.com>
2022-04-07 17:12:39 +0900

    spring Basic

--- core/src/main/java/plo/core/AppConfig.java
+++ core/src/main/java/plo/core/AppConfig.java
@@ -2,7 +2,10 @@ package plo.core;
 
 import plo.core.Order.OrderService;
 import plo.core.Order.OrderServiceImpl;
+import plo.core.discount.DiscountPolicy;
 import plo.core.discount.FixDiscountPolicy;
+import plo.core.discount.RateDiscountPolicy;
+import plo.core.member.MemberRepository;
 import plo.core.member.MemberService;
 import plo.core.member.MemberServiceImpl;
 import plo.core.member.MemoryMemberRepository;
@@ -11,13 +14,23 @@ import plo.core.member.MemoryMemberRepository;
 //생성한 객체 인스턴스 참조를 생성자를 통해서 연결해준다.
 public class AppConfig {
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+
 
 
 }
--- core/src/main/java/plo/core/OrderApp.java
+++ core/src/main/java/plo/core/OrderApp.java
@@ -2,11 +2,9 @@ package plo.core;
 
 import plo.core.Order.Order;
 import plo.core.Order.OrderService;
-import plo.core.Order.OrderServiceImpl;
 import plo.core.member.Grade;
 import plo.core.member.Member;
 import plo.core.member.MemberService;
-import plo.core.member.MemberServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class OrderApp {
         Member member = new Member(memberId, "spring", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "test", 10000);
+        Order order = orderService.createOrder(memberId, "test", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order.caculatePrice() = " + order.caculatePrice());
